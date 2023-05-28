3324e3e60a8601e3e4007963557f68978d392411
Lorenzo <le522@ic.ac.uk>
2023-05-28 12:22:36 +0100

    cleared up formatting warnings in the file output function, changed PSTATE output presentation to be in line with the spec

--- src/armstate.c
+++ src/armstate.c
@@ -30,21 +30,31 @@ void incrementPC(struct CompState* state) {
 	state->PC += 4;
 }
 
+
+// mutates output to have the correct letters for set flags
+void formatPSTATE(struct CompState* state, char *output) {
+        if (state->PSTATE.N == 1) output[0] = 'N';
+        if (state->PSTATE.Z == 1) output[1] = 'Z';
+        if (state->PSTATE.C == 1) output[2] = 'C';
+        if (state->PSTATE.V == 1) output[3] = 'V';
+}
+
+
+
 /* 
  * this function prints the contents of 
  * the registers to the file specified
  * file is not closed 
  */
 void printContentsTo(FILE *fp, struct CompState* state) {
-	fprintf(fp, "%5s : %.16x \n", "PC",  state->PC);
-	fprintf(fp, "%5s : %.16x \n", "ZR", state->ZR);
-	fprintf(fp, "%5s : %.16x \n", "N", state->PSTATE.N);
-	fprintf(fp, "%5s : %.16x \n", "Z", state->PSTATE.Z);
-	fprintf(fp, "%5s : %.16x \n", "C", state->PSTATE.C);
-	fprintf(fp, "%5s : %.16x \n", "V", state->PSTATE.V);
-	fprintf(fp, "%5s : %.16x \n", "SP",state->SP);
+	fprintf(fp, "%5s : %.16ld \n", "PC",  state->PC);
+	fprintf(fp, "%5s : %.16ld \n", "ZR", state->ZR);
+        char pstateOutput[] = "----";
+        formatPSTATE(state, pstateOutput);
+	fprintf(fp, "%5s : %s \n", "PSTATE", pstateOutput);
+
 	for (int i = 0; i<NUM_GEN_REG; i++) {
-		fprintf(fp, "X%d: %.16x \n", i, state -> Regs[i]);
+		fprintf(fp, "X%d: %.16ld \n", i, state -> Regs[i]);
 
 	}
 }
@@ -64,6 +74,7 @@ int main(void) {
 	fclose(fp);
 	fp = fopen("output2.out", "w");
 	incrementPC(&state);
+	state.PSTATE.C = 1;  // doesn't make logical sense, just for testing
 	printContentsTo(fp, &state);
 	fclose(fp);
 
