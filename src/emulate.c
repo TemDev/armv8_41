e3330e42157099a93e3dc37918381edaae6d77be
Sergazin, Temirlan <temirlan.sergazin22@imperial.ac.uk>
2023-05-31 21:21:11 +0000

    Small changes

--- src/emulate.c
+++ src/emulate.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
 #include <assert.h>
 
-#include "singleMTransfer.h" 
+#include "singleMTransfer.h"
+#define MEM_SIZE 16394
 #define HLT 0x8a000000
 
 int main(int argc, char** argv) {
@@ -19,17 +19,31 @@ int main(int argc, char** argv) {
 		fread(&memory[count], sizeof(char), 1, fp);
 		count++;
 	}
-
-	int inst = memory[0];
-	while (inst == HLT ) { // it is supposed to be != but in this case is in infinite loop
+	
+	struct CompState state;
+	
+	int instruction = memory[state.PC];
+	while (instruction == HLT ) { // it is supposed to be != but in this case is in infinite loop
 		// process this instruction 
 		// update the CompState
 		// and memory corresponding to the
 		// next insturction
 		// get the next instruction back 
 		// to inst value
-	}
-	struct CompState state;
+		char Op0 = (instruction >> 25) & 15;
+		if (Op0 == 8 || Op0 == 9) {
+		    
+		} else if (Op0 == 5 || Op0 == 13) {
+		    
+		} else if (Op0 == 4 || Op0 == 6 || Op0 == 12 || Op0 == 14) {
+		    
+		} else if (Op0 == 10 || Op0 == 11) {
+		    
+		};
+		state.PC += 4;
+		
+	};
+	
 	state.Regs[1] = 2;
 	state.Regs[2] = 3;
 	unsignedImmOffset(&state, 0b11111000010000000001110001000001,memory);
@@ -52,48 +66,4 @@ int main(int argc, char** argv) {
  	fclose(file);
 
 	return EXIT_SUCCESS;
-=======
-#define MEM_SIZE 16394
-#define HLT 0x8a000000
-
-int main(int argc, char** argv) {
-        // checks whether there is exactly one output
-        if( argc != 2 ) {
-                fprintf( stderr, "Usage: cat filename!\n" );
-                exit(1);
-        }
-        FILE *fp = fopen(argv[1], "rb");
-        int *memory = (int*) malloc(MEM_SIZE * sizeof(int));
-        int count = 0;
-        while (!feof(fp) && count < 3) {
-                fread(&memory[count], sizeof(int), 1, fp);
-                printf("%.8x \n", memory[count]);
-                count++;
-        }
-
-        int inst = memory[0];
-        while (inst == HLT ) { // it is supposed to be != but in this case is in infinite loop
-                // process this instruction
-                // update the CompState
-                // and memory corresponding to the
-                // next insturction
-                // get the next instruction back
-                // to inst value
-        }
-
-
-        FILE *file = fopen("hello.out", "w");
-
-        for (int i = 0; i < MEM_SIZE; i++){
-                if (memory[i] != 0) {
-                fprintf(file,"0x%.8x : 0x%.8x \n", i << 2, memory[i]);                                                  }
-        }
-        //closes opened files and frees the memory
-        free(memory);
-        fclose(fp);
-        fclose(file);
-
-        return EXIT_SUCCESS;
->>>>>>> 55578fe4592b99fd3cd9886fe8dc7d178ecf5bb4
-}
-
+};
