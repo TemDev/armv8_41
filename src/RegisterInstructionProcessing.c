3889fc522b5c52195dca36b18726984babc68427
Artiukhov, Ivan <ivan.artiukhov22@imperial.ac.uk>
2023-05-31 18:22:23 +0000

    Improved logic arithmetic miltiply and a few other bits for Register type instructions

--- src/RegisterInstructionProcessing.c
+++ src/RegisterInstructionProcessing.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include "bitwise.c"
+
 #define BIT24 16777216 // == 2 ^ 24
 #define BUTM 268435456 // == 2 ^ 28
-#define BIT2523 41943040 // == 2 ^ 25 + 2 ^ 23
+#define BITshift 50331648 // 2 ^ 25 + 2 ^ 24
 #define BITsf 2147483648 // == 2 ^ 31
 #define BITrd 31 // == 2 ^ 4 + 2 ^ 3 + 2 ^ 2 + 2 ^ 1 + 2 ^ 0
-#define rd 4294967295; // 2 ^ 32 - 1
+#define BITx 32768 // == 2 ^ 15
+#define BITra 31744 // == 2 ^ 14 + 2 ^ 13 + 2 ^ 12 + 2 ^ 11 + 2 ^ 10
+#define BITrn 992 // == 2 ^ 9 + 2 ^ 8 + 2 ^ 7 + 2 ^ 6 + 2 ^ 5
+#define BITrm 2031616 // == 2 ^ 20 + 2 ^ 19 + 2 ^ 18 + 2 ^ 17 + 2 ^ 16
+#define rd 4294967295 // 2 ^ 32 - 1
+#define BIToperand 64512 // 2 ^ 15 + 2 ^ 14 + 2 ^ 13 + 2 ^ 12 + 2 ^ 11 + 2 ^ 10
 #define NUM_GEN_REG 32
 
 struct Flags {
@@ -20,11 +27,11 @@ struct CompState {
   long Regs[NUM_GEN_REG];
 };
 
-void arithmetic(struct CompState* state, int instruction) {
-    const int fst = 166777216; // 2 ^ 24
-    const int shift = 12582912; // 2 ^ 23 + 2 ^ 22
-    const short lst = 2097152; // 2 ^ 21;
-    int Op = ((imm12 & instruction) >> 10) << (12 * ((sh & instruction) >> 31));
+void arithmeticc(struct CompState* state, int instruction) {
+    const long fst = 166777216; // 2 ^ 24
+    const long shift = 12582912; // 2 ^ 23 + 2 ^ 22
+    const long lst = 2097152; // 2 ^ 21;
+    long Op = ((imm12 & instruction) >> 10) << (12 * ((sh & instruction) >> 31));
     char Rn = (rn & instruction) >> 5;
     // Bit wise shift should be included and some things will be added / altered
     char opc = (instruction >> 29) & 3;
@@ -170,45 +177,42 @@ void bics(struct CompState* state, int instruction, char Rn, int Op) {
 
 };
 
-void arithmetic(struct CompState* state, int instruction) {
-    const int sh = 4194304;
-    const int imm12 = 4293280;
-    const short rn = 992;
-    int Op = ((imm12 & instruction) >> 10) << (12 * ((sh & instruction) >> 31));
-    char Rn = (rn & instruction) >> 5;
-    char opc = (instruction >> 29) & 3;
-    switch (opc) {
-        case 3:
-        subs(state, instruction, Rn, Op);
-        break;
-        case 2:
-        sub(state, instruction, Rn, Op);
-        break;
-        case 1:
-        adds(state, instruction, Rn, Op);
-        break;
-        default:
-        add(state, instruction, Rn, Op);
-    }
-};
 
 // Multiply-Add: Rd := Ra + (Rn * Rm)
-int madd(int Ra, int Rn, int Rm) {
+int madd(instruction) {
+    char Ra = BITra & instruction;
+    char Rn = BITrn & instruction;
+    char Rm = BITrm & instruction;
     return Ra + (Rn * Rm);
 }
 
 // Multiply-Sub: Rd := Ra - (Rn * Rm)
-int msub(int Ra, int Rn, int Rm) {
+int msub(instruction) {
+    char Ra = BITra & instruction;
+    char Rn = BITrn & instruction;
+    char Rm = BITrm & instruction;
     return Ra - (Rn * Rm);
 }
 
+void logic(struct CompState* state, int instruction) {
+    if ((instruction >> 22) & 1) {
+        lsr ((instruction & BIToperand) 1) // assumed that all the shifts happen by 1 bit
+    } else if ((instruction >> 22) & 2) {
+        asr ((instruction & BIToperand) 1)
+    } else
+        lsl ((instruction & BIToperand) 1)
+}
 
-void bit-logic(struct CompState* state, int instruction) {
-    
+void bitLogic(struct CompState* state, int instruction) {
+    ror ((instruction & BIToperand) 1)
 };
 
 void multiply(struct CompState* state, int instruction) {
-    
+  if (instruction & BITx) {
+      state->Regs[Rd] = msub(instruction);
+  } else {
+      state->Regs[Rd] = madd(instruction);
+  }
 };
 
 // Determines type of immediate instruction, arithmetic or wideMove.
@@ -216,7 +220,7 @@ void determineType(struct CompState* state, int instruction) {
     if (instruction & !(BITM) & (BIT24)) {
         arithmetic(state, instruction);
     } else if (instruction & !(BITM) & !(BIT24) {
-        bit-logic(state, instruction);
+        logic(state, instruction);
         else if (instruction & (BITM) & (BIT24)) {
             multiply(state, instruction);
         }
