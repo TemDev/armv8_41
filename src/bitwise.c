046ae1addd5ac846f293d43a0e6abe05e012e3c4
Evans, Lorenzo <lorenzo.evans22@imperial.ac.uk>
2023-05-31 18:22:40 +0000

    changed 32 bit functions to ensure they are correct, fixed problems with logical right shift and rotate right

--- src/bitwise.c
+++ src/bitwise.c
@@ -1,60 +1,66 @@
 #include <stdio.h>
+#include <stdint.h>
 #define BIT_MASK_32 ((1UL << 32) - 1)
 
-int lsl_64(int operand, int shift_amount) 
+int64_t lsl_64(int64_t operand, int shift_amount)
 { 
     return operand << shift_amount;
 }
 
-int lsr_64(int operand, int shift_amount) 
-{ 
-    return (signed) (((unsigned) operand) >> shift_amount);  // only if unsigned
+int64_t lsr_64(int64_t operand, int shift_amount)
+{
+    return ((uint64_t) operand) >> shift_amount;
 }
 
-int asr_64(int operand, int shift_amount) 
+int64_t asr_64(int64_t operand, int shift_amount)
 { 
-    return operand >> shift_amount;  // only if signed
+    return operand >> shift_amount;
 }
 
-int ror_64(int operand, int shift_amount) 
+int64_t ror_64(int64_t operand, int shift_amount)
 { 
     int mask = (1 << shift_amount) - 1;
-    int rotated_bits = operand & mask;
-    int new_bits = rotated_bits << (64 - shift_amount);
-    return (signed) (((unsigned) lsl_64(operand, shift_amount)) + new_bits);
+    int64_t rotated_bits = operand & mask;
+    int64_t new_bits = rotated_bits << (64 - shift_amount);
+    return (int64_t) (((uint64_t) lsl_64(operand, shift_amount)) + new_bits);
 }
 
-int lsl_32(int operand, int shift_amount)
+int32_t lsl_32(int64_t operand, int shift_amount)
 {
-    return lsl_64(operand & BIT_MASK_32, shift_amount);
+    return lsl_64(operand, shift_amount) & BIT_MASK_32;
 }
 
-int lsr_32(int operand, int shift_amount)
+int32_t lsr_32(int64_t operand, int shift_amount)
 {
-    return lsr_64(operand & BIT_MASK_32, shift_amount);
+//    return lsr_64(operand & BIT_MASK_32, shift_amount);
+    return ((uint32_t) operand) >> shift_amount;
 }
 
-int asr_32(int operand, int shift_amount)
+int32_t asr_32(int64_t operand, int shift_amount)
 {
-    return asr_64(operand & BIT_MASK_32, shift_amount);
+//    return asr_64(operand, shift_amount) & BIT_MASK_32;
+    return ((int32_t) operand) >> shift_amount;
 }
 
-int ror_32(int operand, int shift_amount)
+int32_t ror_32(int64_t operand, int shift_amount)
 {
-    return ror_64(operand & BIT_MASK_32, shift_amount);
+    int mask = (1 << shift_amount) - 1;
+    int32_t rotated_bits = operand & mask;
+    int32_t new_bits = rotated_bits << (32 - shift_amount);
+    return (int32_t) (((uint32_t) lsl_32(operand, shift_amount)) + new_bits);
 }
 
 int main(void) {
-	long testvals[] = {21, -21, 51294967296, -5194967296};
+    int64_t testvals[] = {21, -21, 51294967296, -5194967296};
 	for(int i = 0; i < 4; i++) {
-		int x = testvals[i];
-		printf("logical LS of %d is %d\n", x, lsl_64(x, 1));
+        int64_t x = testvals[i];
+		printf("logical LS of %ld is %ld\n", x, lsl_64(x, 1));
 		printf("using 32 bit mode, it is %d\n\n", lsl_32(x, 1));
-		printf("arithmetic RS of %d is %d\n", x, asr_64(x, 1));
-		printf("using 32 bit mode, it is  %d\n\n", asr_32(x, 1));
-		printf("logical RS of %d is %d\n", x, lsl_64(x, 1));
-		printf("using 32 bit mode, it is  %d\n\n", lsl_32(x, 1));
-		printf("rotation R of %d is %d\n", x, ror_64(x, 1));
-		printf("using 32 bit mode, it is  %d\n\n-----------------------------------\n\n", ror_32(x, 1));
+		printf("arithmetic RS of %ld is %ld\n", x, asr_64(x, 1));
+		printf("using 32 bit mode, it is %d\n\n", asr_32(x, 1));
+		printf("logical RS of %ld is %ld\n", x, lsr_64(x, 1));
+		printf("using 32 bit mode, it is %d\n\n", lsr_32(x, 1));
+		printf("rotation R of %ld is %ld\n", x, ror_64(x, 1));
+		printf("using 32 bit mode, it is %d\n\n-----------------------------------\n\n", ror_32(x, 1));
 	}
 }
