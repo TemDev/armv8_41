1178ca318990d4e25969299e2f406e7826841bdc
Sergazin, Temirlan <temirlan.sergazin22@imperial.ac.uk>
2023-06-01 19:20:16 +0000

    Small corrections to logic

--- src/branchInstructions.c
+++ src/branchInstructions.c
@@ -2,15 +2,13 @@
 #include <math.h>
 #include "branchInstructions.h"
 
-#define BIT2826 335544320 // == 2 ^ 28 + 2 ^ 26
-#define BIT313029282726 4227858432 // == 2 ^ 31 + 2 ^ 30 + 2 ^ 29 + 2 ^ 28 + 2 ^ 27 + 2 ^ 26
 #define BIT31302826252019181716 3592355840 // == 2 ^ 31 + 2 ^ 30 + 2 ^ 28 + 2 ^ 26 + 2 ^ 25 + 2 ^ 20 + 2 ^ 19 + 2 ^ 18 + 2 ^ 17 + 2 ^ 16
 #define NOTBIT98765 4294966303 // == bitwise_not(2 ^ 9 + 2 ^ 8 + 2 ^ 7 + 2 ^ 6 + 2 ^ 5)
 #define BIT98765 992 // == 2 ^ 9 + 2 ^ 8 + 2 ^ 7 + 2 ^ 6 + 2 ^ 5
 #define BIT31302928272625244 4278190096 // == 2 ^ 31 + 2 ^ 30 + 2 ^ 29 + 2 ^ 28 + 2 ^ 27 + 2 ^ 26 + 2 ^ 25 + 2 ^ 24 + 2 ^ 4
 #define BIT302826 1409286144 // == 2 ^ 30 + 2 ^ 28 + 2 ^ 26
 #define BIT26MASK 67108863 // == 2 ^ 26 - 1
-#define BIT235MASK 16777184 // == 2 ^ 24 - 2 ^ 5
+#define BIT19MASK 524287// == 2 ^ 19 - 1
 #define BIT4MASK 15 // == 2 ^ 4 - 1
 
 static void unconditional(struct CompState* state, long simm26) {
@@ -64,14 +62,14 @@ static void conditional(struct CompState* state, long offset, char cond) {
 };
 
 void branch(struct CompState* state, int instruction) {
-    if ((instruction & BIT313029282726) == BIT2826) {
+    if ((instruction >> 26) == 5) {
         long simm26 = instruction & BIT26MASK;
         unconditional(state, simm26);
     } else if ((instruction & NOTBIT98765) == BIT31302826252019181716) {
         char Rn = (instruction & BIT98765) >> 5;
         registerBranch(state, Rn);
     } else if ((instruction & BIT31302928272625244) == BIT302826) {
-        long simm19 = instruction & BIT235MASK;
+        long simm19 = (instruction >> 5) & BIT19MASK;
         char cond = instruction & BIT4MASK;
         conditional(state, simm19 * 4, cond);
     };
