a6ad364d2953384ff88da720bb95b129eea59e2c
Sergazin, Temirlan <temirlan.sergazin22@imperial.ac.uk>
2023-06-01 19:09:10 +0000

    Simplified code + Corrected the way flags are supposed to be set and operations dealt with

--- src/immediateInstructionProcessing.c
+++ src/immediateInstructionProcessing.c
@@ -2,16 +2,14 @@
 #include <math.h>
 #include "emulate.h"
 #include "immediateInstructionProcessing.h"
+#include "bitwise.h"
 
 #define BIT24 16777216 // == 2 ^ 24, MASK
 #define BIT2523 41943040 // == 2 ^ 25 + 2 ^ 23, MASK
 #define BITsf 2147483648 // == 2 ^ 31, MASK
 #define BITrd 31 // == 2 ^ 4 + 2 ^ 3 + 2 ^ 2 + 2 ^ 1 + 2 ^ 0, MASK
 #define BIT32MASK 4294967295 // == 2 ^ 32 - 1, MASK
-#define BIT64MASK 18446744073709551615 // == 2 ^ 64 - 1, MASK
-#define BIT64 18446744073709551616 // == 2 ^ 64, MASK
 #define REGISTER31 31
-#define BIT63 9223372036854775808 // == 2 ^ 63, MASK
 #define BIT31 2147483648 // == 2 ^ 31, MASK
 #define BIT32 4294967296 // == 2 ^ 32, MASK
 
@@ -19,40 +17,23 @@ static void add(struct CompState* state, int instruction, char Rn, int Op) {
     char Rd = BITrd & instruction;
     
     if (BITsf & instruction) {
-        long long result;
+        long result;
         if (Rn == REGISTER31) {
             result = state->SP + Op;
+            state->SP = result;
         } else {
             result = state->Regs[Rn] + Op;
-        };
-        
-        if (Rd == REGISTER31) {
-            if (result < 0) {
-                state->SP = result;
-            } else {
-                state->SP = result & BIT64MASK;
-            };
-        } else {
-            state->Regs[Rd] = result & BIT64MASK;
+            state->Regs[Rd] = result;
         };
         
     } else {
-        long result;
+        int result;
         if (Rn == REGISTER31) {
-            result = (state->SP & BIT32MASK) + Op;
+            result = ((int) (state->SP & BIT32MASK)) + Op;
+            state->SP = (state->SP & 0) ^ result;
         } else {
             result = (state->Regs[Rn] & BIT32MASK) + Op;
-        };
-        
-        if (Rd == REGISTER31) {
-            if (result < 0) {
-                int res = result;
-                state->SP = res;
-            } else {
-                state->SP = result & BIT32MASK;
-            };
-        } else {
-            state->Regs[Rd] = result & BIT32MASK;
+            state->Regs[Rn] = (state->Regs[Rn] & 0) ^ result;
         };
     };
 };
@@ -61,121 +42,37 @@ static void adds(struct CompState* state, int instruction, char Rn, int Op) {
     char Rd = BITrd & instruction;
     
     if (BITsf & instruction) {
-        long long result;
+        long result;
         if (Rn == REGISTER31) {
-            result = state->ZR + Op;
+            result = state->SP + Op;
+            state->PSTATE.V = (state->SP > 0 && Op > 0 && result < 0) || (state->SP < 0 && Op < 0 && result > 0);
+            state->PSTATE.C = (state->SP < 0 && Op < 0) || (state->SP < 0 && Op > 0 && result >= 0) || (state->SP > 0 && Op < 0 && result >= 0);
+            state->SP = result;
         } else {
             result = state->Regs[Rn] + Op;
+            state->PSTATE.V = (state->Regs[Rd] > 0 && Op > 0 && result < 0) || (state->Regs[Rd] < 0 && Op < 0 && result > 0);
+            state->PSTATE.C = (state->Regs[Rd] < 0 && Op < 0) || (state->Regs[Rd] < 0 && Op > 0 && result >= 0) || (state->Regs[Rd] > 0 && Op < 0 && result >= 0);
+            state->Regs[Rd] = result;
         };
-        
-        //Set Flags
-        state->PSTATE.N = result < 0 ? true : false;
-        state->PSTATE.Z = result == 0 ? true : false;
-        if (result > 0) {
-            state->PSTATE.C = result & BIT64 ? true : false;
-        } else {
-            state->PSTATE.C = false;
-        };
-        state->PSTATE.V = state->PSTATE.C;
-        if (!(Rd == REGISTER31)) {
-            if (result < 0) {
-                state->Regs[Rd] = result;
-            } else {
-                state->Regs[Rd] = result & BIT64MASK;
-            };
-        };
+        state->PSTATE.N = result < 0;
+        state->PSTATE.Z = result == 0;
         
     } else {
-        long result;
+        int result;
         if (Rn == REGISTER31) {
-            result = (state->ZR & BIT32MASK) + Op;
+            result = ((int) (state->SP & BIT32MASK)) + Op;
+            state->PSTATE.V = (state->SP > 0 && Op > 0 && result < 0) || (state->SP < 0 && Op < 0 && result > 0);
+            state->PSTATE.C = (state->SP < 0 && Op < 0) || (state->SP < 0 && Op > 0 && result >= 0) || (state->SP > 0 && Op < 0 && result >= 0);
+            state->SP = (state->SP & 0) ^ result;
         } else {
             result = (state->Regs[Rn] & BIT32MASK) + Op;
+            state->PSTATE.V = (state->Regs[Rd] > 0 && Op > 0 && result < 0) || (state->Regs[Rd] < 0 && Op < 0 && result > 0);
+            state->PSTATE.C = (state->Regs[Rd] < 0 && Op < 0) || (state->Regs[Rd] < 0 && Op > 0 && result >= 0) || (state->Regs[Rd] > 0 && Op < 0 && result >= 0);
+            state->Regs[Rn] = (state->Regs[Rn] & 0) ^ result;
         };
-        
-        //Set Flags
-        state->PSTATE.N = result < 0 ? true : false;
-        state->PSTATE.Z = result == 0 ? true : false;
-        if (result > 0) {
-            state->PSTATE.C = result & BIT32 ? true : false;
-        } else {
-            state->PSTATE.C = false;
-        };
-        state->PSTATE.V = state->PSTATE.C;
-        
-        if (!(Rd == REGISTER31)) {
-            if (result < 0) {
-                int res = result;
-                state->Regs[Rd] = res;
-            } else {
-                state->Regs[Rd] = result & BIT32MASK;
-            };
-        };
-    };
-};
-
-static void subs(struct CompState* state, int instruction, char Rn, int Op) {
-    char Rd = BITrd & instruction;
-    
-    if (BITsf & instruction) {
-        long long result;
-        if (Rn == REGISTER31) {
-            result = state->SP - Op;
-        } else {
-            result = state->Regs[Rn] - Op;
-        };
-        
-        //Set Flags
-        state->PSTATE.N = result < 0 ? true : false;
-        state->PSTATE.Z = result == 0 ? true : false;
-        if (result > 0) {
-            state->PSTATE.C = result & BIT64 ? true : false;
-        } else {
-            if (result < -BIT63) {
-                state->PSTATE.C = true;
-            } else {
-                state->PSTATE.C = false;
-            };
-        };
-        state->PSTATE.V = state->PSTATE.C;
-        if (!(Rd == REGISTER31)) {
-            if (result < 0) {
-                state->Regs[Rd] = result;
-            } else {
-                state->Regs[Rd] = result & BIT64MASK;
-            };
-        };
-        
-    } else {
-        long result;
-        if (Rn == REGISTER31) {
-            result = (state->SP & BIT32MASK) - Op;
-        } else {
-            result = (state->Regs[Rn] & BIT32MASK) - Op;
-        };
-        
-        //Set Flags
-        state->PSTATE.N = result < 0 ? true : false;
-        state->PSTATE.Z = result == 0 ? true : false;
-        if (result > 0) {
-            state->PSTATE.C = result & BIT32 ? true : false;
-        } else {
-            if (result < -BIT31) {
-                state->PSTATE.C = true;
-            } else {
-                state->PSTATE.C = false;
-            };
-        };
-        state->PSTATE.V = state->PSTATE.C;
-        
-        if (!(Rd == REGISTER31)) {
-            if (result < 0) {
-                int res = result;
-                state->Regs[Rd] = res;
-            } else {
-                state->Regs[Rd] = result & BIT32MASK;
-            };
-        };
+        state->PSTATE.C = state->PSTATE.V;
+        state->PSTATE.N = result < 0;
+        state->PSTATE.Z = result == 0;
     };
 };
 
@@ -188,7 +85,7 @@ static void arithmeticImmediate(struct CompState* state, int instruction) {
     char opc = (instruction >> 29) & 3;
     switch (opc) {
         case 3:
-        subs(state, instruction, Rn, Op);
+        adds(state, instruction, Rn, (-Op));
         break;
         case 2:
         add(state, instruction, Rn, (-Op));
@@ -252,6 +149,6 @@ void determineTypeImmediate(struct CompState* state, int instruction) {
     };
 };
 
-static void main() {
+void main() {
     
 };
