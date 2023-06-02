c99fa07e17bc15a25dd8cb168c387db1d2d2f535
Evans, Lorenzo <lorenzo.evans22@imperial.ac.uk>
2023-06-02 13:50:00 +0000

    Updated to work with 64 bit signed numbers

--- src/test_functions.c
+++ src/test_functions.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "test_functions.h"
 
-void printBits( uint32_t x, int nbits ) {
-	uint_32 mask = 1 << (nbits-1);
+void printBits( int64_t x, int nbits ) {
+	uint_64 mask = 1 << (nbits-1);
 	for( int i=0; i<nbits; i++ ) {
 		int bit = (x & mask) != 0;
 		putchar( '0' + bit );
