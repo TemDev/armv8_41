4a40b5d0f7320eaf3eecd863635099e8002a1af6
Lorenzo <le522@ic.ac.uk>
2023-06-03 19:46:30 +0100

    fixed compilation errors

--- src/bitwise.c
+++ src/bitwise.c
@@ -4,7 +4,7 @@
 #define BIT_MASK_32 ((1UL << 32) - 1)
 
 int64_t lsl_64(int64_t operand, int shift_amount)
-{ 
+{
     return operand << shift_amount;
 }
 
@@ -14,12 +14,12 @@ int64_t lsr_64(int64_t operand, int shift_amount)
 }
 
 int64_t asr_64(int64_t operand, int shift_amount)
-{ 
+{
     return operand >> shift_amount;
 }
 
 int64_t ror_64(int64_t operand, int shift_amount)
-{ 
+{
     int mask = (1 << shift_amount) - 1;
     int64_t rotated_bits = operand & mask;
     int64_t new_bits = rotated_bits << (64 - shift_amount);
@@ -51,7 +51,7 @@ int32_t ror_32(int64_t operand, int shift_amount)
     return (int32_t) (((uint32_t) lsl_32(operand, shift_amount)) + new_bits);
 }
 
-static int main(void) {
+int main(void) {
     int64_t testvals[] = {21, -21, 51294967296, -5194967296};
     char *function_names[] = {"logical LS", "arithmetic RS", "logical RS", "rotation"};
     typedef int64_t (*func_pointer_64)(int64_t, int);
@@ -59,17 +59,17 @@ static int main(void) {
     typedef int32_t (*func_pointer_32)(int64_t, int);
     func_pointer_32 shift_functions_32[] = {&lsl_32, &asr_32, &lsr_32, &ror_32};
 	for(int i = 0; i < 4; i++) {
-        int64_t x = testvals[i];
-        for(int j = 0; j < 4; j++) {
-            printf("%s of %ld is %ld\n", function_names[j] x, shift_functions_64[j](x, 1));
-            printBits(x, 64);
-            printBits(shift_functions_64[j](x, 1), 64)
-            printf("\nusing 32 bit mode, it is %d\n", shift_functions_32[j](x, 1));
-            printBits(x, 32);
-            printBits(shift_functions_32[j](x, 1), 32)
-            printf("\n");
-        }
-        printf("-------------------------------------------\n")
+        	int64_t x = testvals[i];
+        	for(int j = 0; j < 4; j++) {
+            		printf("%s of %ld is %ld\n", function_names[j], x, shift_functions_64[j](x, 1));
+            		printBits(x, 64);
+            		printBits(shift_functions_64[j](x, 1), 64);
+            		printf("\nusing 32 bit mode, it is %d\n", shift_functions_32[j](x, 1));
+            		printBits(x, 32);
+            		printBits(shift_functions_32[j](x, 1), 32);
+            		printf("\n");
+        	}
+        	printf("-------------------------------------------\n");
 		// printf("logical LS of %ld is %ld\n", x, lsl_64(x, 1));
         // printBits(x, 64);
         // printBits(lsl_64(x, 1), 64)
@@ -81,6 +81,7 @@ static int main(void) {
 		// printf("logical RS of %ld is %ld\n", x, lsr_64(x, 1));
 		// printf("using 32 bit mode, it is %d\n\n", lsr_32(x, 1));
 		// printf("rotation R of %ld is %ld\n", x, ror_64(x, 1));
-		// printf("using 32 bit mode, it is %d\n\n-----------------------------------\n\n", ror_32(x, 1));
+		// printf("using 32 bit mode, it is %d\n\n-----------------------------------\n\n", ror_32(x, 1));;
 	}
+	return 0;
 }
