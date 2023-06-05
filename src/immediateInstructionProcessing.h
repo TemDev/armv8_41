18a23390bda47baaee00b013a48585fbfa5cccc7
ia1022 <ia1022@ic.ac.uk>
2023-06-06 00:52:34 +0100

    Modified and fixed the header file

--- src/immediateInstructionProcessing.h
+++ src/immediateInstructionProcessing.h
@@ -2,4 +2,4 @@
 
 extern void determineTypeImmediate(struct CompState* state, int instruction);
 extern void add(struct CompState* state, int instruction, char Rn, long Op);
-extern void adds(struct CompState* state, int instruction, char Rn, long Op);
+extern void adds(struct CompState* state, int instruction, char Rn, long Op, char subOrNot);
