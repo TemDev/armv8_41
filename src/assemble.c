1b9770b861d0c84f6e539f1463d4671e77cbf268
Sergazin, Temirlan <temirlan.sergazin22@imperial.ac.uk>
2023-06-08 18:23:37 +0000

    Handled the output of an array into a binary file using sophisticated and modern approaches.

--- src/assemble.c
+++ src/assemble.c
@@ -1,4 +1,13 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#define MAX_INSTRUCTIONS 100
+
+void writeFile(int32_t *arr, char *file) {
+  FILE *file_out = fopen(file, "wb");
+  fwrite(arr, sizeof(int32_t), MAX_INSTRUCTIONS, file_out);
+  fclose(file_out);
+}
 
 int main(int argc, char **argv) {
   if (argc == 3) {
