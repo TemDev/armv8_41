42e3a40bbfd85a0f3c177c6960f397cd1b0b8687
Dionisii Nuzhnyi <dn622@ic.ac.uk>
2023-06-14 16:34:03 +0100

    Changed insertReg so it works correctly

--- src/Aliases.c
+++ src/Aliases.c
@@ -4,18 +4,15 @@
 #include "Aliases.h"
 
 operand* insertReg(instruction_data *ins, operand elem, int pos){
-    operand *ops = calloc(ins -> no_operands, sizeof(operand));
+    operand *ops = calloc(ins -> no_operands + 1, sizeof(operand));
     memcpy(ops, ins -> operands, ins -> no_operands * sizeof(operand));
-    for (int i = 0 ; i < strlen(ops); i++)
+    for (int i = ins -> no_operands - 1 ; i > pos - 1 ; i--)
     {
-        if ( i > pos) {
-        ops[i] = ops[i - 1] ;
-        }
-        else {
-            if ( i == pos)
-            ops[i] = elem;
+        if ( i >= pos) {
+        ops[i + 1] = ops[i] ;
         }
     }
+    ops[pos] = elem;
     return ops;
 }
 
@@ -48,39 +45,39 @@ instruction_data convert(instruction_data *inst) {
     // Check if the instruction is "cmp" and has two operands
     if (strcmp(inst->instruction_name, "cmp") == 0) {
         // Construct the "subs" instruction using the operands
-        refined = (instruction_data){"subs", insertReg(inst->operands, mode, 0), inst->no_operands + 1};
+        refined = (instruction_data){"subs", insertReg(inst, mode, 0), inst->no_operands + 1};
 
     } else if (strcmp(inst->instruction_name, "cmn") == 0) {
         // Construct the "adds" instruction using the operands
-        refined = (instruction_data){"adds", insertReg(inst->operands, mode, 0), inst->no_operands + 1};
+        refined = (instruction_data){"adds", insertReg(inst, mode, 0), inst->no_operands + 1};
 
     } else if (strcmp(inst->instruction_name, "neg") == 0) {
         // Construct the "sub" instruction using the operands
-        refined = (instruction_data){"sub", insertReg(inst->operands, mode, 1), inst->no_operands + 1};
+        refined = (instruction_data){"sub", insertReg(inst, mode, 1), inst->no_operands + 1};
 
     } else if (strcmp(inst->instruction_name, "negs") == 0) {
         // Construct the "subs" instruction using the operands
-        refined = (instruction_data){"subs", insertReg(inst->operands, mode, 1), inst->no_operands + 1};
+        refined = (instruction_data){"subs", insertReg(inst, mode, 1), inst->no_operands + 1};
 
     } else if (strcmp(inst->instruction_name, "tst") == 0) {
         // Construct the "ands" instruction using the operands
-        refined = (instruction_data){"ands", insertReg(inst->operands, mode, 0), inst->no_operands + 1};
+        refined = (instruction_data){"ands", insertReg(inst, mode, 0), inst->no_operands + 1};
 
     } else if (strcmp(inst->instruction_name, "mvn") == 0) {
         // Construct the "orn" instruction using the operands
-        refined = (instruction_data){"orn", insertReg(inst->operands, mode, 1), inst->no_operands + 1};
+        refined = (instruction_data){"orn", insertReg(inst, mode, 1), inst->no_operands + 1};
 
     } else if (strcmp(inst->instruction_name, "mov") == 0) {
         // Construct the "orr" instruction using the operands
-        refined = (instruction_data){"orr", insertReg(inst->operands, mode, 1), inst->no_operands + 1};
+        refined = (instruction_data){"orr", insertReg(inst, mode, 1), inst->no_operands + 1};
 
     } else if (strcmp(inst->instruction_name, "mul") == 0) {
         // Construct the "madd" instruction using the operands
-        refined = (instruction_data){"madd", insertReg(inst->operands, mode, 3), inst->no_operands + 1};
+        refined = (instruction_data){"madd", insertReg(inst, mode, 3), inst->no_operands + 1};
 
     } else if (strcmp(inst->instruction_name, "mneg") == 0) {
         // Construct the "msub" instruction using the operands
-        refined = (instruction_data){"msub", insertReg(inst->operands, mode, 3), inst->no_operands + 1};
+        refined = (instruction_data){"msub", insertReg(inst, mode, 3), inst->no_operands + 1};
 
     } else {
         refined = *inst; // in case the instruction is invalid or not a aliase
