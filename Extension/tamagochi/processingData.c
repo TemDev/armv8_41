4d5d40c8419b67654f4213872b598d680e0ac775
Evans, Lorenzo <lorenzo.evans22@imperial.ac.uk>
2023-06-21 19:38:06 +0000

    created new basic loop - should be moved to main and connected

--- Extension/tamagochi/processingData.c
+++ Extension/tamagochi/processingData.c
@@ -1,1 +1,43 @@
 // get data
+// #define isCloudy !isSunny;
+
+int temp;  // assume data recieved (degrees C)
+int waterlevel;  // assume data recieved [0,1]
+int lightLevel;  // assume data recieved [0,1]
+
+bool precipitation
+bool heavyPrecipitation;
+bool tooHot;
+bool tooCold;
+
+
+void resetEnvironment(void) {
+    precipitation = false;
+    heavyPrecipitation = false;
+    tooHot = false;
+    tooCold = false;
+}
+
+while (true) {
+    // update sensor values
+    resetEnvironment();
+    if (temp > 20) {
+        tooHot = true;
+    } else if (temp < 0) {
+        tooCold = true;
+    }
+    if (waterlevel > 0.1) {
+        precipitation = true;
+    } else if (waterlevel > 0.7) {
+        heavyPrecipitation = true;
+    }
+    // wait 10 seconds
+}
+
+/* CHECKS IN MAIN:
+sunny = tooHot ^ ¬(prec ^ heavyPrec)
+rain = prec ^ ¬tooCold
+thunder = heavyPrec ^ ¬tooCold
+snow = (prec | heavyPrec) ^ tooCold
+
+*/
