99a978250b72a2a5243cb44b361ba97da21f837b
Dionisii Nuzhnyi <dn622@ic.ac.uk>
2023-06-22 11:49:23 +0100

    Connected with attempted music background

---
+++ Extension/tamagochi/musicBackground.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sndfile.h>
+#include <math.h>
+#include "defs.h"
+
+#define NUM_WAVES 40
+
+
+float* readData(char * path);
+
+
+static init(void);
+
+static addNew(float value);
+
+
+Texture2D getMusicBackground(Texture2D* t,float time, float* buffer, Color background);
